782b8bca801df60ef7592bc0e61173c5782a41e0
martinadre <martina.dre@centrum.cz>
2022-02-22 19:13:48 +0100

    1 file modified, 2 added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*import './ukol1';
 import './ukol2';*/
 
-let cislo = Number(prompt('Kolik je ti let?');
-console.log('Je ti tolik let: ' + cislo);
+import './ukol3'
---
+++ ukol3.js
@@ -0,0 +1,14 @@
+/*Přijměte jméno jako vstup od uživatele, dále nechte 
+zadat jeho věk a vypište na základě věku jednu ze 
+dvou vět ve tvaru:
+a) Vaše jméno je Marian, je vám 30 let a tak máte 
+právo řídit auto.
+b) Vaše jméno je Marian, je vám 14 let a tak musíte 
+počkat ještě 4 let, než budete moci řídit auto*/
+
+let jmeno = prompt("Napiš své jméno")
+let vek = Number(prompt(Napiš svůj věk"))
+
+if (vek=30) {"Vaše jméno je " + jmeno + " a máte právo řídit auto."}
+
+
