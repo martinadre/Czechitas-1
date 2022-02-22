fe0d06e61c8fa809396b9a22e1bc8afbe2edf52f
martinadre <martina.dre@centrum.cz>
2022-02-22 18:04:21 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,5 +1,15 @@
-let a = 5;
-let b = 10;
+/*Založte proměnné pro jméno, věk, váhu v kg a  výšku  v metrech - nastavte jim libovolnou hodnotu.
+Spočtěte BMI
+Do konzole vypište větu ve formátu:
 
-let soucet = a + b + 100;
-console.log(a / 100);
+Jmenuji se Petra, je mi 34 let, vážím 65kg,  měřím 1.67m a moje BMI je 23,2.
+*/
+
+let jmeno = 'Martina';
+let vek = 36;
+let vaha = 62;
+let vyska = 1.76;
+
+let BMI = vaha / vyska;
+
+console.log(BMI);
