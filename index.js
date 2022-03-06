b635140a0753fe2110c35586ffd9fa1f25d4801e
martinadre <martina.dre@centrum.cz>
2022-03-06 21:55:18 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //import './funkce1';
 
-/*let x = 0;
+/*
+let x = 0;
 
 while (x < 5) {
   console.log('Ahoj');
@@ -9,4 +10,14 @@ while (x < 5) {
 }
 */
 
-console.log('Ahoj');
+
+
+/*
+let cislo = 0;
+
+while (cislo < 10) {
+  console.log('Slava Ukrajine');
+
+  cislo = cislo + 1;
+}
+*/
